aedcbcc92b28bea4d7c6fe1517bdcb5afc57cdfa
PFernanadez <pedro.nacho997@gmail.com>
2016-06-07 19:05:28 -0300

    Add files via upload

---
+++ ProgramaSuru/ProgramaSuru/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmInicio
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmInicio))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Titulo = New System.Windows.Forms.Label()
+        Me.btnMinimizar = New System.Windows.Forms.PictureBox()
+        Me.btnCerrar = New System.Windows.Forms.PictureBox()
+        Me.Logo = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.btnMinimizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnCerrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.Titulo)
+        Me.Panel1.Controls.Add(Me.btnMinimizar)
+        Me.Panel1.Controls.Add(Me.btnCerrar)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(559, 23)
+        Me.Panel1.TabIndex = 0
+        '
+        'Titulo
+        '
+        Me.Titulo.AutoSize = True
+        Me.Titulo.Font = New System.Drawing.Font("Agency FB", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Titulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Titulo.Location = New System.Drawing.Point(27, 1)
+        Me.Titulo.Name = "Titulo"
+        Me.Titulo.Size = New System.Drawing.Size(60, 18)
+        Me.Titulo.TabIndex = 3
+        Me.Titulo.Text = "Suru - Inicio"
+        '
+        'btnMinimizar
+        '
+        Me.btnMinimizar.BackColor = System.Drawing.Color.Transparent
+        Me.btnMinimizar.Image = CType(resources.GetObject("btnMinimizar.Image"), System.Drawing.Image)
+        Me.btnMinimizar.Location = New System.Drawing.Point(522, 2)
+        Me.btnMinimizar.Name = "btnMinimizar"
+        Me.btnMinimizar.Size = New System.Drawing.Size(14, 14)
+        Me.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnMinimizar.TabIndex = 2
+        Me.btnMinimizar.TabStop = False
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.BackColor = System.Drawing.Color.Transparent
+        Me.btnCerrar.Image = CType(resources.GetObject("btnCerrar.Image"), System.Drawing.Image)
+        Me.btnCerrar.Location = New System.Drawing.Point(543, 2)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(14, 14)
+        Me.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnCerrar.TabIndex = 1
+        Me.btnCerrar.TabStop = False
+        '
+        'Logo
+        '
+        Me.Logo.BackColor = System.Drawing.Color.Transparent
+        Me.Logo.Image = CType(resources.GetObject("Logo.Image"), System.Drawing.Image)
+        Me.Logo.Location = New System.Drawing.Point(4, 1)
+        Me.Logo.Name = "Logo"
+        Me.Logo.Size = New System.Drawing.Size(18, 18)
+        Me.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Logo.TabIndex = 1
+        Me.Logo.TabStop = False
+        '
+        'frmInicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(560, 360)
+        Me.Controls.Add(Me.Logo)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.Name = "frmInicio"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.btnMinimizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnCerrar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Titulo As System.Windows.Forms.Label
+    Friend WithEvents Logo As System.Windows.Forms.PictureBox
+    Friend WithEvents btnMinimizar As System.Windows.Forms.PictureBox
+    Friend WithEvents btnCerrar As System.Windows.Forms.PictureBox
+End Class
---
+++ ProgramaSuru/ProgramaSuru/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class frmInicio
+    Dim NewPoint As New System.Drawing.Point
+    Dim X, Y As Integer
+
+
+    Private Sub Panel1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseDown
+        X = Control.MousePosition.X - Me.Location.X
+        Y = Control.MousePosition.X - Me.Location.Y
+    End Sub
+    Private Sub Panel1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            NewPoint = Control.MousePosition
+            NewPoint.X -= (X)
+            NewPoint.Y -= (Y)
+            Me.Location = NewPoint
+        End If
+    End Sub
+    Private Sub btnCerrar_MouseDown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.MouseDown
+        Me.btnCerrar.Image = My.Resources.BtB
+    End Sub
+    Private Sub btnCerrar_MouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.MouseEnter
+        Me.btnCerrar.Image = My.Resources.BtnC
+    End Sub
+    Private Sub btnCerrar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.MouseLeave
+        Me.btnCerrar.Image = My.Resources.BtB
+    End Sub
+    Private Sub btnCerrar_MouseUp(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.MouseUp
+        Me.btnCerrar.Image = My.Resources.BtnC
+    End Sub
+    Private Sub btnMinimizar_MouseDown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMinimizar.MouseDown
+        Me.btnMinimizar.Image = My.Resources.BtB
+    End Sub
+    Private Sub btnMinimizar_MouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMinimizar.MouseEnter
+        Me.btnMinimizar.Image = My.Resources.BtnM
+    End Sub
+    Private Sub btnMinimizar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMinimizar.MouseLeave
+        Me.btnMinimizar.Image = My.Resources.BtB
+    End Sub
+    Private Sub btnMinimizar_MouseUp(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMinimizar.MouseUp
+        Me.btnMinimizar.Image = My.Resources.BtnM
+    End Sub
+
+    Private Sub btnMinimizar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub btnCerrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Titulo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Titulo.Click
+
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
